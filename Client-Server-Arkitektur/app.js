17b02a1390ebd53c0747c7d34c332e61b1d221f4
alla2297 <alla2297@edu.easj.dk>
2022-12-13 18:31:11 +0100

    udkriver vejret af de 5 dage

--- Client-Server-Arkitektur/app.js
+++ Client-Server-Arkitektur/app.js
@@ -88,19 +88,119 @@ app.post('/weather-sites', (req, res) => {
             dayChange += 8;
             week[a] = weekday;
         }
+        const date = new Date();
+        let day = date.getDate()+1;
+        let arrayOfTheFirst8Indexs =[weekday[0].todayDate[8]+weekday[0].todayDate[9],
+            weekday[1].todayDate[8]+weekday[1].todayDate[9],
+            weekday[2].todayDate[8]+weekday[2].todayDate[9],
+            weekday[3].todayDate[8]+weekday[3].todayDate[9],
+            weekday[4].todayDate[8]+weekday[4].todayDate[9],
+            weekday[5].todayDate[8]+weekday[5].todayDate[9],
+            weekday[6].todayDate[8]+weekday[6].todayDate[9],
+            weekday[7].todayDate[8]+weekday[7].todayDate[9]
+        ];
+        console.log(day,'welcome',p[2]);
+        if (day==arrayOfTheFirst8Indexs[0]) {
+            res.render('weather-sites', { title: 'Weather Overview', 
+                day1: weekday[8].todayWeather,day1temp: weekday[8].todayTemp, 
+                day2: weekday[16].todayWeather,day2temp: weekday[16].todayTemp,
+                day3: weekday[21].todayWeather,day3temp: weekday[21].todayTemp, 
+                day4: weekday[29].todayWeather,day4temp: weekday[29].todayTemp, 
+                day5: weekday[37].todayWeather,day5temp: weekday[37].todayTemp
+            });
+        }else if (day==arrayOfTheFirst8Indexs[1]) {
+            let index =1;
+            res.render('weather-sites', { title: 'Weather Overview', 
+                day1: weekday[0].todayWeather,day1temp: weekday[0].todayTemp, 
+                day2: weekday[index+8].todayWeather,day2temp: weekday[index+8].todayTemp,
+                day3: weekday[index+16].todayWeather,day3temp: weekday[index+16].todayTemp, 
+                day4: weekday[index+21].todayWeather,day4temp: weekday[index+21].todayTemp, 
+                day5: weekday[index+29].todayWeather,day5temp: weekday[index+29].todayTemp
+            });
+            
+        }else if (day==arrayOfTheFirst8Indexs[2]) {
+            let index =2;
+            res.render('weather-sites', { title: 'Weather Overview', 
+                day1: weekday[0].todayWeather,day1temp: weekday[0].todayTemp, 
+                day2: weekday[index].todayWeather,day2temp: weekday[index].todayTemp,
+                day3: weekday[index+8].todayWeather,day3temp: weekday[index+8].todayTemp, 
+                day4: weekday[index+16].todayWeather,day4temp: weekday[index+16].todayTemp, 
+                day5: weekday[index+21].todayWeather,day5temp: weekday[index+21].todayTemp
+            });
+        }else if (day==arrayOfTheFirst8Indexs[3]) {
+            let index=3;
+            res.render('weather-sites', { title: 'Weather Overview', 
+                day1: weekday[0].todayWeather,day1temp: weekday[0].todayTemp, 
+                day2: weekday[index].todayWeather,day2temp: weekday[index].todayTemp,
+                day3: weekday[index+8].todayWeather,day3temp: weekday[index+8].todayTemp, 
+                day4: weekday[index+16].todayWeather,day4temp: weekday[index+16].todayTemp, 
+                day5: weekday[index+21].todayWeather,day5temp: weekday[index+21].todayTemp
+            });
+        }else if (day==arrayOfTheFirst8Indexs[4]) {
+            let index=4;
+            res.render('weather-sites', { title: 'Weather Overview', 
+                day1: weekday[0].todayWeather,day1temp: weekday[0].todayTemp, 
+                day2: weekday[index].todayWeather,day2temp: weekday[index].todayTemp,
+                day3: weekday[index+8].todayWeather,day3temp: weekday[index+8].todayTemp, 
+                day4: weekday[index+16].todayWeather,day4temp: weekday[index+16].todayTemp, 
+                day5: weekday[index+21].todayWeather,day5temp: weekday[index+21].todayTemp
+            });
+            
+        }else if (day==arrayOfTheFirst8Indexs[5]) {
+            let index=5;
+            res.render('weather-sites', { title: 'Weather Overview', 
+                day1: weekday[0].todayWeather,day1temp: weekday[0].todayTemp, 
+                day2: weekday[index].todayWeather,day2temp: weekday[index].todayTemp,
+                day3: weekday[index+8].todayWeather,day3temp: weekday[index+8].todayTemp, 
+                day4: weekday[index+16].todayWeather,day4temp: weekday[index+16].todayTemp, 
+                day5: weekday[index+21].todayWeather,day5temp: weekday[index+21].todayTemp
+            });
+            
+        }else if (day==arrayOfTheFirst8Indexs[6]) {
+            let index=6;
+            res.render('weather-sites', { title: 'Weather Overview', 
+                day1: weekday[0].todayWeather,day1temp: weekday[0].todayTemp, 
+                day2: weekday[index].todayWeather,day2temp: weekday[index].todayTemp,
+                day3: weekday[index+8].todayWeather,day3temp: weekday[index+8].todayTemp, 
+                day4: weekday[index+16].todayWeather,day4temp: weekday[index+16].todayTemp, 
+                day5: weekday[index+21].todayWeather,day5temp: weekday[index+21].todayTemp
+            });
+            
+        }else if (day==arrayOfTheFirst8Indexs[7]) {
+            let index=7;
+            res.render('weather-sites', { title: 'Weather Overview', 
+                day1: weekday[0].todayWeather,day1temp: weekday[0].todayTemp, 
+                day2: weekday[index].todayWeather,day2temp: weekday[index].todayTemp,
+                day3: weekday[index+8].todayWeather,day3temp: weekday[index+8].todayTemp, 
+                day4: weekday[index+16].todayWeather,day4temp: weekday[index+16].todayTemp, 
+                day5: weekday[index+21].todayWeather,day5temp: weekday[index+21].todayTemp
+            });
+            
+        }else{
+            res.render('weather-sites', { title: 'Weather Overview', 
+                day1: 'Erro',day1temp:'', 
+                day2: 'Erro',day2temp:'', 
+                day3: 'Erro',day3temp:'', 
+                day4: 'Erro',day4temp:'', 
+                day5: 'Erro',day5temp:''
+            });
+        }
+
         
+        
+        let index1=2;
         console.log ("hey( ",weekday[0].todayWeather,'\n',weekday[0].todayDate,' )hey___________________________________hey',
-        '\n',weekday[1].todayWeather,'\n',weekday[1].todayDate,'\n',
-        '\n',weekday[2].todayWeather,'\n',weekday[2].todayDate,'\n',
-        '\n',weekday[3].todayWeather,'\n',weekday[3].todayDate,'\n');
-        res.render('weather-sites', 
+        '\n',weekday[index1].todayWeather,'\n',weekday[index1].todayDate,'\n',
+        '\n',weekday[index1+8].todayWeather,'\n',weekday[index1+8].todayDate,'\n',
+        '\n',weekday[index1+16].todayWeather,'\n',weekday[index1+16].todayDate,'\n');
+        /*res.render('weather-sites', 
         { title: 'Weather Overview', 
             day1: weekday[0].todayWeather,day1temp: weekday[0].todayTemp, 
             day2: weekday[1].todayWeather,day2temp: weekday[1].todayTemp,
             day3: weekday[2].todayWeather,day3temp: weekday[2].todayTemp, 
             day4: weekday[3].todayWeather,day4temp: weekday[3].todayTemp, 
             day5: weekday[4].todayWeather,day5temp: weekday[4].todayTemp
-        });
+        });*/
     }) 
     .catch(err => console.log(err));
 });
